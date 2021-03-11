6da1f0e126a1a2ceb39a2c28473b870328ad1344
baldwalker <insigniadesign@yahoo.co.uk>
2021-03-11 14:14:14 +0000

    Update values.py

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -166,11 +166,21 @@ IGNORED_FINGERPRINTS = [CAR.VELOSTER, CAR.GENESIS_G70, CAR.KONA]
 
 FW_VERSIONS = {
   CAR.IONIQ_PHEV_2020: {
-    (Ecu.fwdRadar, 0x7d0, None): [b'\xf1\x00AEhe SCC FHCUP      1.00 1.02 99110-G2100         ', ],
-    (Ecu.engine, 0x7e0, None): [b'\xf1\x816H6F6051\x00\x00\x00\x00\x00\x00\x00\x00', ],
-    (Ecu.eps, 0x7d4, None): [b'\xf1\x00AE  MDPS C 1.00 1.01 56310/G2510 4APHC101', ],
-    (Ecu.fwdCamera, 0x7c4, None): [b'\xf1\x00AEP MFC  AT USA LHD 1.00 1.01 95740-G2600 190819', ],
-    (Ecu.transmission, 0x7e1, None): [b'\xf1\x816U3J9051\x00\x00\xf1\x006U3H1_C2\x00\x006U3J9051\x00\x00PAE0G16NL0\x82zT\xd2', ],
+    (Ecu.fwdRadar, 0x7d0, None): [
+      b'\xf1\x00AEhe SCC FHCUP      1.00 1.02 99110-G2100         ',
+    ],
+    (Ecu.engine, 0x7e0, None): [
+      b'\xf1\x816H6F6051\x00\x00\x00\x00\x00\x00\x00\x00',
+    ],
+    (Ecu.eps, 0x7d4, None): [
+      b'\xf1\x00AE  MDPS C 1.00 1.01 56310/G2510 4APHC101',
+    ],
+    (Ecu.fwdCamera, 0x7c4, None): [
+      b'\xf1\x00AEP MFC  AT USA LHD 1.00 1.01 95740-G2600 190819',
+    ],
+    (Ecu.transmission, 0x7e1, None): [
+      b'\xf1\x816U3J9051\x00\x00\xf1\x006U3H1_C2\x00\x006U3J9051\x00\x00PAE0G16NL0\x82zT\xd2',
+    ],
   },
   CAR.IONIQ_EV_2020: {
     (Ecu.fwdRadar, 0x7d0, None): [
